7d1b57cfc3ee1dcd58c10ee0ddea7f764c82bc4a
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 23:05:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,18 @@
 def reverse_each_word(string)
   array = string.split(" ")
+  
   new_array = array.collect { |x|
     x.reverse
   }
-  
   
   
   #new_array = []
   #array.each { |x|
   #  new_array << x.reverse  
   #}
+  
+  
+  
   new_array.join(" ")
 end
 
