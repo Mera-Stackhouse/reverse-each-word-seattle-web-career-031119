944e155ecc211bafe55e79ebe4b2ef45659d4ba4
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 23:05:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  
+  new_array = array.collect { |x|
+    x.reverse
+  }
+  
+  
+  #new_array = []
+  #array.each { |x|
+  #  new_array << x.reverse  
+  #}
+  
+  
+  
+  new_array.join(" ")
+end
+
+# string = "Hello, mera"
+
+# puts reverse_each_word(string)
